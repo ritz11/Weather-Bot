c6c7eaeead656a3d24feb5674f741cb67bf5bd01
ritz11 <ra99ag@gmail.com>
2017-10-20 17:46:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,6 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
     baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
@@ -59,9 +57,13 @@ def processRequest(req):
     yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
+    
+    if req.get("result").get("action") == "yahooWeatherForecast":
+        res = makeWebhookResult(data)
+        return res
+    if req.get("result").get("action") == "sunset":
+        res = makeWebhookResult1(data)
+        return res
 
 def makeYqlQuery(req):
     result = req.get("result")
@@ -100,6 +102,43 @@ def makeWebhookResult(data):
 
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+def makeWebhookResult1(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    astronomy = channel.get('astronomy')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + "sun will set at" + astronomy.get("sunset")
 
     print("Response:")
     print(speech)
